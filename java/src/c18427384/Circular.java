221d180eadfd7ebaa1546460fd9a3f35cdc228ce
Daraghm99 <daraghm15@gmail.com>
2020-05-15 16:56:13 +0100

    Spheres inside the Cube

--- java/src/c18427384/Circular.java
+++ java/src/c18427384/Circular.java
@@ -5,7 +5,9 @@ import ie.tudublin.Visual;
 public class Circular extends Visual {
     
     boolean dub = false;
+    boolean triple = false;
     boolean quad = false;
+    boolean complete = false;
 
 
     public void settings()
@@ -28,7 +30,13 @@ public class Circular extends Visual {
             dub = ! dub;
         }
         if(key == '2'){
+            triple = ! triple;
+        }
+        if(key == '3'){
             quad = ! quad;
+        }
+        if(key == '4'){
+            complete = ! complete;
         }
     }
 
@@ -46,6 +54,16 @@ public class Circular extends Visual {
 
         //Sphere and cube will double
         if(dub){
+            render_dub();
+        }
+        if(triple){
+            render_triple();
+        }
+        if(quad){
+            render_quad();     
+        }
+        if(complete){
+            render_complete();
             float sphereSize = 50 + (100 * getAmplitude());
             pushMatrix();
             translate(0, 0, -300);
@@ -73,7 +91,35 @@ public class Circular extends Visual {
             popMatrix();
             angle += 0.01f;
 
+            //extra sphere will populate the left edge of the cube
+            pushMatrix();
+            translate(-325, 0, -700);
+            rotateX(angle);
+            rotateY(angle);
+            rotateY(angle);             
+            sphere(sphereSize);
+            popMatrix();
+            angle += 0.01f;
 
+            //extra sphere will populate the top of the cube
+            pushMatrix();
+            translate(0, -325, -700);
+            rotateX(angle);
+            rotateY(angle);
+            rotateY(angle);             
+            sphere(sphereSize);
+            popMatrix();
+            angle += 0.01f;
+
+            //extra sphere will populate the bottom
+            pushMatrix();
+            translate(0, 325, -700);
+            rotateX(angle);
+            rotateY(angle);
+            rotateY(angle);             
+            sphere(sphereSize);
+            popMatrix();
+            angle += 0.01f;
         }
         else{
             //A rotating Sphere
@@ -95,6 +141,189 @@ public class Circular extends Visual {
             popMatrix();
 
         }
-    }     
+
+    }
+
+    private void render_dub() {
+        float sphereSize = 50 + (100 * getAmplitude());
+        pushMatrix();
+        translate(0, 0, -300);
+        rotateX(angle);
+        rotateY(angle);
+        rotateY(angle);             
+        sphere(sphereSize);
+        popMatrix();
+        angle += 0.01f;
+
+        //drawing a cube to hold the sphere in
+        float boxSize = 100 + (200 * getAmplitude());
+        pushMatrix();
+        translate(0, 0, -100);
+        box(boxSize);
+        popMatrix();
+
+        //extra sphere will populate the right edge of the cube
+        pushMatrix();
+        translate(325, 0, -700);
+        rotateX(angle);
+        rotateY(angle);
+        rotateY(angle);             
+        sphere(sphereSize);
+        popMatrix();
+        angle += 0.01f;
+    }
+
+    private void render_triple(){
+
+        float sphereSize = 50 + (100 * getAmplitude());
+        pushMatrix();
+        translate(0, 0, -300);
+        rotateX(angle);
+        rotateY(angle);
+        rotateY(angle);             
+        sphere(sphereSize);
+        popMatrix();
+        angle += 0.01f;
+
+        //drawing a cube to hold the sphere in
+        float boxSize = 100 + (200 * getAmplitude());
+        pushMatrix();
+        translate(0, 0, -100);
+        box(boxSize);
+        popMatrix();
+
+        //extra sphere will populate the right edge of the cube
+        pushMatrix();
+        translate(325, 0, -700);
+        rotateX(angle);
+        rotateY(angle);
+        rotateY(angle);             
+        sphere(sphereSize);
+        popMatrix();
+        angle += 0.01f;
+
+        //extra sphere will populate the left edge of the cube
+        pushMatrix();
+        translate(-325, 0, -700);
+        rotateX(angle);
+        rotateY(angle);
+        rotateY(angle);             
+        sphere(sphereSize);
+        popMatrix();
+        angle += 0.01f;
+    }
+
+    private void render_quad(){
+  
+        float sphereSize = 50 + (100 * getAmplitude());
+        pushMatrix();
+        translate(0, 0, -300);
+        rotateX(angle);
+        rotateY(angle);
+        rotateY(angle);             
+        sphere(sphereSize);
+        popMatrix();
+        angle += 0.01f;
+
+        //drawing a cube to hold the sphere in
+        float boxSize = 100 + (200 * getAmplitude());
+        pushMatrix();
+        translate(0, 0, -100);
+        box(boxSize);
+        popMatrix();
+
+       //extra sphere will populate the right edge of the cube
+        pushMatrix();
+        translate(325, 0, -700);
+        rotateX(angle);
+        rotateY(angle);
+        rotateY(angle);             
+        sphere(sphereSize);
+        popMatrix();
+        angle += 0.01f;
+
+        //extra sphere will populate the left edge of the cube
+        pushMatrix();
+        translate(-325, 0, -700);
+        rotateX(angle);
+        rotateY(angle);
+        rotateY(angle);             
+        sphere(sphereSize);
+        popMatrix();
+        angle += 0.01f;
+
+        //extra sphere will populate the bottom of the cube
+        pushMatrix();
+        translate(0, -325, -700);
+        rotateX(angle);
+        rotateY(angle);
+        rotateY(angle);             
+        sphere(sphereSize);
+        popMatrix();
+        angle += 0.01f;          
+
+    }
+
+    private void render_complete(){
+    
+        float sphereSize = 50 + (100 * getAmplitude());
+        pushMatrix();
+        translate(0, 0, -300);
+        rotateX(angle);
+        rotateY(angle);
+        rotateY(angle);             
+        sphere(sphereSize);
+        popMatrix();
+        angle += 0.01f;
+
+        //drawing a cube to hold the sphere in
+        float boxSize = 100 + (200 * getAmplitude());
+        pushMatrix();
+        translate(0, 0, -100);
+        box(boxSize);
+        popMatrix();
+
+       //extra sphere will populate the right edge of the cube
+        pushMatrix();
+        translate(325, 0, -700);
+        rotateX(angle);
+        rotateY(angle);
+        rotateY(angle);             
+        sphere(sphereSize);
+        popMatrix();
+        angle += 0.01f;
+
+        //extra sphere will populate the left edge of the cube
+        pushMatrix();
+        translate(-325, 0, -700);
+        rotateX(angle);
+        rotateY(angle);
+        rotateY(angle);             
+        sphere(sphereSize);
+        popMatrix();
+        angle += 0.01f;
+
+        //extra sphere will populate the top of the cube
+        pushMatrix();
+        translate(0, -325, -700);
+        rotateX(angle);
+        rotateY(angle);
+        rotateY(angle);             
+        sphere(sphereSize);
+        popMatrix();
+        angle += 0.01f;
+
+        //extra sphere will populate the bottom
+        pushMatrix();
+        translate(0, 325, -700);
+        rotateX(angle);
+        rotateY(angle);
+        rotateY(angle);             
+        sphere(sphereSize);
+        popMatrix();
+        angle += 0.01f;      
+
+
+    }
 
 }
