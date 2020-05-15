4dd33d1e7674e007e1695123b0b92a811ca2088a
Daraghm99 <daraghm15@gmail.com>
2020-05-15 13:21:58 +0100

    sphereRotation

--- java/src/c18427384/Circular.java
+++ java/src/c18427384/Circular.java
@@ -18,20 +18,25 @@ public class Circular extends Visual {
         getAudioPlayer().play();
     }
 
+
+    float angle = 0;
     public void draw(){
         
         calculateAverageAmplitude();
         background(0);
-        noFill();
+        //noFill();
         lights();
         stroke(map(getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
         strokeWeight(5);
         pushMatrix();
         camera(0, 0, 0, 0, 0, -1, 0, 1, 0);
-        translate(0, 0, -200);
-        float sphereSize = 50 + (200 * getAmplitude());               
+        translate(0, 0, -300);
+        rotateX(angle);
+        rotateZ(angle);
+        float sphereSize = 25 + (100 * getAmplitude());               
         sphere(sphereSize);
         popMatrix();
+        angle += 1;
 
     }
 
